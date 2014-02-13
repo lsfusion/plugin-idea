7ec4f6ba318e3949c3581a5a254ba1260417d3f9
pavel-miniutka <asphalt.by@gmail.com>
2014-02-13 11:53:39 +0000

    ctrl+alt+shift+N для свойств: имя + классы параметров
    git-svn-id: svn://81.25.47.234/lsfusion/plugin-idea@50 dcdcfa73-82ac-7b44-9532-e36433290fed

--- src/com/simpleplugin/psi/LSFProjectComponent.java
+++ src/com/simpleplugin/psi/LSFProjectComponent.java
@@ -1,9 +1,17 @@
 package com.simpleplugin.psi;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupManager;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.impl.PsiManagerEx;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.simpleplugin.psi.declarations.LSFGlobalPropDeclaration;
+import com.simpleplugin.psi.stubs.types.indexes.PropIndex;
 import org.jetbrains.annotations.NotNull;
 
 public class LSFProjectComponent implements ProjectComponent {
@@ -19,6 +27,29 @@ public class LSFProjectComponent implements ProjectComponent {
             public void run() {
                 LSFResolveCache.getParamClassesInstance().clearCache(true);
                 LSFResolveCache.getValueClassInstance().clearCache(true);
+            }
+        });
+
+        StartupManager.getInstance(project).runWhenProjectIsInitialized(new Runnable() {
+            @Override
+            public void run() {
+                // прогоняем ресолвинг классов параметров свойств на старте, чтобы не терять время на парсинг потом, в частности на Ctrl+Alt+Shift+N
+                ProgressManager.getInstance().run(new Task.Backgroundable(project, "Resolving properties param classes") {
+                    @Override
+                    public void run(@NotNull final ProgressIndicator indicator) {
+                        ApplicationManager.getApplication().runReadAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                PropIndex propIndex = PropIndex.getInstance();
+                                for (String key : propIndex.getAllKeys(project)) {
+                                    for (LSFGlobalPropDeclaration decl : propIndex.get(key, project, GlobalSearchScope.projectScope(project))) {
+                                        decl.resolveParamClasses();
+                                    }
+                                }
+                            }
+                        });
+                    }
+                });
             }
         });
     }
