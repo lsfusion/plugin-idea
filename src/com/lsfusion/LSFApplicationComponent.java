32d3985d8042501e50df91120c3d94fddde9b886
Pavel Miniutka <9590440+pavel-miniutka@users.noreply.github.com>
2017-04-04 06:49:37 +0000

    фикс совместимости

--- src/com/lsfusion/LSFApplicationComponent.java
+++ src/com/lsfusion/LSFApplicationComponent.java
@@ -1,7 +1,7 @@
 package com.lsfusion;
 
 import com.intellij.openapi.components.ApplicationComponent;
-import io.netty.util.internal.PlatformDependent;
+import org.apache.commons.lang.SystemUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class LSFApplicationComponent implements ApplicationComponent {
@@ -9,7 +9,7 @@ public class LSFApplicationComponent implements ApplicationComponent {
     public void initComponent() {
         // попытка исправить падающий иногда IllegalArgumentException, связанный с TimSort. исправлено в Java9
         // http://bugs.java.com/bugdatabase/view_bug.do?bug_id=7075600
-        if (PlatformDependent.javaVersion() < 9) {
+        if (SystemUtils.JAVA_VERSION_FLOAT < 1.9) {
             System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
         }
     }
