dd63768bf5b79437240e85b68251ce772f5932b5
Pavel Miniutka <9590440+pavel-miniutka@users.noreply.github.com>
2018-02-16 11:48:28 +0000

    не создаём автоматически контейнеры для ряда групп свойств

--- src/com/lsfusion/design/model/AbstractGroup.java
+++ src/com/lsfusion/design/model/AbstractGroup.java
@@ -1,9 +1,13 @@
 package com.lsfusion.design.model;
 
+import java.util.Arrays;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 public class AbstractGroup {
+    private static final List<String> NO_CONTAINER_GROUPS = Arrays.asList("root", "public", "private", "base", "recognize", "drilldown", "propertyPolicy"); 
+    
     public String caption;
     public boolean createContainer = true;
     public String sID;
@@ -15,6 +19,9 @@ public class AbstractGroup {
     public AbstractGroup(String sID, String caption) {
         this.sID = sID;
         this.caption = caption;
+        if (NO_CONTAINER_GROUPS.contains(sID)) {
+            createContainer = false;
+        }
     }
 
     public void add(AbstractGroup child) {
