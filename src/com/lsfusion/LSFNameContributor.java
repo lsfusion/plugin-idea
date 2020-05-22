20f6be3243c1daf8a91ae0b05a02104038942477
alex-byi <alexeybyimistrov@gmail.com>
2020-05-22 16:53:26 +0300

    Make symbol contributor asynchronous

--- src/com/lsfusion/LSFNameContributor.java
+++ src/com/lsfusion/LSFNameContributor.java
@@ -1,7 +1,6 @@
 package com.lsfusion;
 
-import com.intellij.ide.util.gotoByName.ChooseByNameBase;
-import com.intellij.navigation.ChooseByNameContributor;
+import com.intellij.navigation.ChooseByNameContributorEx;
 import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ex.WindowManagerEx;
@@ -9,67 +8,69 @@ import com.intellij.psi.codeStyle.MinusculeMatcher;
 import com.intellij.psi.codeStyle.NameUtil;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.stubs.StringStubIndexExtension;
+import com.intellij.util.Processor;
+import com.intellij.util.indexing.FindSymbolParameters;
+import com.intellij.util.indexing.IdFilter;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
-public abstract class LSFNameContributor implements ChooseByNameContributor {
+public abstract class LSFNameContributor implements ChooseByNameContributorEx {
     protected abstract Collection<StringStubIndexExtension> getIndices();
 
     // it's hacky here, but all that pattern obtaining is a big hack
     // the problem is that when you press CTRL+SHIFT+N - editor is focused, so we can't get pattern from there, but we'll assume that there will be previous pattern (default idea behaviour)
     private static String prevPattern = null;
-    @NotNull
+
     @Override
-    public String[] getNames(Project project, boolean includeNonProjectItems) {
-        String pattern = null;
-        final Component focusedComponent = WindowManagerEx.getInstanceEx().getFocusedComponent(project);
+    public void processNames(@NotNull Processor<? super String> processor, @NotNull GlobalSearchScope scope, @Nullable IdFilter filter) {
+        String pattern;
+        final Component focusedComponent = WindowManagerEx.getInstanceEx().getFocusedComponent(scope.getProject());
         if (focusedComponent instanceof JTextField) {
             pattern = ((JTextField) focusedComponent).getText();
             prevPattern = pattern;
         } else {
             pattern = prevPattern;
         }
-        
-        List<String> result = new ArrayList<>();
         for (StringStubIndexExtension index : getIndices()) {
-            result.addAll(getIndexKeys(index, pattern, project, includeNonProjectItems));
+            processIndexKey(index, pattern, scope.getProject(), true, processor);
         }
-        return result.toArray(new String[result.size()]);
     }
 
-    protected Collection<String> getIndexKeys(StringStubIndexExtension index, String pattern, Project project, boolean includeNonProjectItems) {
-        return index.getAllKeys(project);
-    }
-
-    @NotNull
     @Override
-    public NavigationItem[] getItemsByName(String name, String pattern, Project project, boolean includeNonProjectItems) {
-        final GlobalSearchScope scope = includeNonProjectItems ? GlobalSearchScope.allScope(project) : GlobalSearchScope.projectScope(project);
+    public void processElementsWithName(@NotNull String name, @NotNull Processor<? super NavigationItem> processor, @NotNull FindSymbolParameters parameters) {
 
-        final List<NavigationItem> result = new ArrayList<>();
         for (StringStubIndexExtension index : getIndices()) {
-            result.addAll(getItemsFromIndex(index, name, project, scope));
+            Collection<NavigationItem> navigationItems = getItemsFromIndex(index, name, parameters.getProject(), parameters.getSearchScope());
+            for (NavigationItem itemsFromIndex : navigationItems) {
+                processor.process(itemsFromIndex);
+            }
         }
-        return result.toArray(new NavigationItem[result.size()]);
+    }
+
+    protected void processIndexKey(StringStubIndexExtension index, String pattern, Project project,
+                                   boolean includeNonProjectItems, Processor<? super String> processor) {
+        Collection<String> allKeys = index.getAllKeys(project);
+        for (String allKey : allKeys) {
+            processor.process(allKey);
+        }
     }
 
     protected Collection<NavigationItem> getItemsFromIndex(StringStubIndexExtension index, String name, Project project, GlobalSearchScope scope) {
         return index.get(name, project, scope);
     }
-    
+
     protected boolean matches(String name, String pattern) {
         MinusculeMatcher totalMatcher = NameUtil.buildMatcher(pattern, NameUtil.MatchingCaseSensitivity.NONE);
         if (totalMatcher.matches(name)) {
             return true;
         }
-        
+
         String[] words = pattern.split("(?=\\p{Upper})");
-        
+
         String cutPattern = pattern;
         for (int i = words.length - 1; i > 0; i--) {
             String word = words[i];
@@ -79,7 +80,6 @@ public abstract class LSFNameContributor implements ChooseByNameContributor {
                 return true;
             }
         }
-        
         return false;
     }
 }
--- src/com/lsfusion/LSFSymbolContributor.java
+++ src/com/lsfusion/LSFSymbolContributor.java
@@ -5,6 +5,10 @@ import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.stubs.StringStubIndexExtension;
+import com.intellij.psi.stubs.StubIndex;
+import com.intellij.psi.stubs.StubIndexKey;
+import com.intellij.util.Processor;
+import com.intellij.util.indexing.IdFilter;
 import com.lsfusion.lang.classes.LSFClassSet;
 import com.lsfusion.lang.psi.declarations.LSFActionOrGlobalPropDeclaration;
 import com.lsfusion.lang.psi.indexes.*;
@@ -17,12 +21,12 @@ public class LSFSymbolContributor extends LSFNameContributor {
     // полагаем, что состояние не меняется между вызовами getNames() и getItemsByName()
     // оба вызова осуществляются в рамках одного метода. (см. DefaultChooseByNameItemProvider.filterElements(), SearchEverywhereAction.buildSymbols())
     private final Map<String, List<NavigationItem>> propertyDeclarationsMap = new HashMap<>();
-    
+
     private void clearPropertyDeclarationsMap() {
         synchronized (propertyDeclarationsMap) {
             propertyDeclarationsMap.clear();
         }
-            
+
     }
 
     private List<NavigationItem> getPropertyDeclarationsMap(String withParams, boolean putIfIsEmpty) {
@@ -56,11 +60,11 @@ public class LSFSymbolContributor extends LSFNameContributor {
 
     @NotNull
     @Override
-    public String[] getNames(Project project, boolean includeNonProjectItems) {
+    public void processNames(@NotNull Processor<? super String> processor, @NotNull GlobalSearchScope scope, @Nullable IdFilter filter) {
         clearPropertyDeclarationsMap();
-        return super.getNames(project, includeNonProjectItems);
+        super.processNames(processor, scope, filter);
     }
-    
+
     private static class ActionOrPropFullNavigationItem implements NavigationItem {
         private final String fullName;
         private final NavigationItem decl;
@@ -99,11 +103,12 @@ public class LSFSymbolContributor extends LSFNameContributor {
     }
 
     @Override
-    protected Collection<String> getIndexKeys(StringStubIndexExtension index, String pattern, Project project, boolean includeNonProjectItems) {
+    protected void processIndexKey(StringStubIndexExtension index, String pattern, Project project,
+                                   boolean includeNonProjectItems, Processor<? super String> processor) {
+
+        final GlobalSearchScope scope = includeNonProjectItems ? GlobalSearchScope.allScope(project) : GlobalSearchScope.projectScope(project);
         if (index instanceof ActionOrPropIndex) {
-            List<String> result = new ArrayList<>();
             Collection<String> allKeys = index.getAllKeys(project);
-            final GlobalSearchScope scope = includeNonProjectItems ? GlobalSearchScope.allScope(project) : GlobalSearchScope.projectScope(project);
             for (String key : allKeys) {
                 if (pattern != null && !matches(key, pattern)) {
                     continue;
@@ -122,12 +127,11 @@ public class LSFSymbolContributor extends LSFNameContributor {
                     String withParams = key + paramsString;
                     List<NavigationItem> decls = getPropertyDeclarationsMap(withParams, true);
                     decls.add(new ActionOrPropFullNavigationItem(withParams, decl));
-                    result.add(withParams);
+                    processor.process(withParams);
                 }
             }
-            return result;
         } else {
-            return super.getIndexKeys(index, pattern, project, includeNonProjectItems);
+            super.processIndexKey(index, pattern, project, includeNonProjectItems, processor);
         }
     }
 
