140d630c536f03baa0bccbe320e2b001078938aa
pavel-miniutka <asphalt.by@gmail.com>
2020-10-21 09:54:03 +0300

    highlight element under cursor fixes (params & form property)

--- src/com/lsfusion/LSFTargetElementEvaluatorEx2.java
+++ src/com/lsfusion/LSFTargetElementEvaluatorEx2.java
@@ -4,15 +4,23 @@ import com.intellij.codeInsight.TargetElementEvaluatorEx2;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.lsfusion.lang.psi.LSFExprParameterUsage;
 import com.lsfusion.lang.psi.declarations.LSFDeclaration;
+import com.lsfusion.lang.psi.declarations.LSFPropertyDrawDeclaration;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class LSFTargetElementEvaluatorEx2 extends TargetElementEvaluatorEx2 {
     @Override
     public boolean isAcceptableNamedParent(@NotNull PsiElement parent) {
+        LSFExprParameterUsage paramUsageParent = PsiTreeUtil.getParentOfType(parent, LSFExprParameterUsage.class);
+        if (paramUsageParent != null) {
+            return false;
+        }
         LSFDeclaration declParent = PsiTreeUtil.getParentOfType(parent, LSFDeclaration.class);
-        return declParent != null && parent.equals(declParent.getNameIdentifier());
+        return declParent != null &&
+                !(declParent instanceof LSFPropertyDrawDeclaration) &&
+                parent.equals(declParent.getNameIdentifier());
     }
 
     // for backward compatibility
