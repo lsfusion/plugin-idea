f172a9afd4111058075cee4543ab284b3f19f219
AFilipchyk <filipchyk.andrei@gmail.com>
2023-01-18 14:50:26 +0300

    ImplementationsSearch: under ProgressManager.getInstance().runProcess

--- src/com/lsfusion/ImplementationsSearch.java
+++ src/com/lsfusion/ImplementationsSearch.java
@@ -2,6 +2,9 @@ package com.lsfusion;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.QueryExecutorBase;
+import com.intellij.openapi.progress.PerformInBackgroundOption;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.Processor;
@@ -17,11 +20,11 @@ public class ImplementationsSearch extends QueryExecutorBase<PsiElement, PsiElem
             final LSFDeclaration declParent = PsiTreeUtil.getParentOfType(sourceElement, LSFDeclaration.class);
 
             if (declParent != null) {
-                ApplicationManager.getApplication().runReadAction(() -> {
+                ApplicationManager.getApplication().runReadAction(() -> ProgressManager.getInstance().runProcess(() -> {
                     for (PsiElement element : declParent.processImplementationsSearch()) {
                         consumer.process(element);
                     }
-                });
+                }, new BackgroundableProcessIndicator(sourceElement.getProject(), "Implementations Search", PerformInBackgroundOption.ALWAYS_BACKGROUND,  "cancel", "stop", false)));
             }
         }
     }
